84c91fa1a769daf4075ddbc9d2b3015a837dd1b0
Harish Narayanan <harish@simula.no>
2012-09-26 15:33:57 +0200

    Ignore some selected albums

--- fetch-albums.py
+++ fetch-albums.py
@@ -4,23 +4,34 @@ import gdata.geo
 
 gd_client = gdata.photos.service.PhotosService()
 gd_client.source = 'org-harishnarayanan-album-fetcher'
-user_name = 'hnarayanan'
+
 google_plus_user_id = '100382636415340600164'
+ignored_albums = ('5716491723391976337', '5706446377771654129',
+                  '5688661881733842001', '5668520065107350593',
+                  '5625206640295281457', '5625205598890785729')
 
 
-albums = gd_client.GetUserFeed(user=user_name)
+albums = gd_client.GetUserFeed(user=google_plus_user_id)
+
+for album in albums.entry:
+    if album.gphoto_id.text not in ignored_albums:
+        print 'title: %s, number of photos: %s, id: %s' % (album.title.text,
+                                                           album.numphotos.text,
+                                                           album.gphoto_id.text)
 
 #print '<table>'
 
-for album in albums.entry:
+
+
+#for album in albums.entry:
 #    print '<tr>'
 
 #    print 'Album: %s (%s)' % (album.title.text, album.numphotos.text)
 
-    photos = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (google_plus_user_id, album.gphoto_id.text))
-    for photo in photos.entry:
+#    photos = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (google_plus_user_id, album.gphoto_id.text))
+#    for photo in photos.entry:
 #        print '<td>'
-        print '<a href="%s"><img src="%s" /></a>' % ( photo.media.thumbnail[0].url, photo.content.src )
+#        print '<a href="%s"><img src="%s" /></a>' % ( photo.media.thumbnail[0].url, photo.content.src )
 #        print '</td>'
 #    print '</tr>'
 
